057dd51b452ea8e96d6a715098261c65b7e2a903
adriana <nxs5899@rit.edu>
2016-10-30 19:47:23 -0400

    new copy

---
+++ sam2017 - Copy/samapp/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
+# Register your models here.
+
+
+from .models import Author, Paper
+from .forms import AuthorForm, PaperForm
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Author
+
+class PaperAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Paper
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Paper)
